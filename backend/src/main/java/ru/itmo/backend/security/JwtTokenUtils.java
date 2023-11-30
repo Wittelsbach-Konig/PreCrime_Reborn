b5137533b2d075a7e4a7979f015056d00d9d26d3
Кирюшин Виталий <kiryuschin.vitalij@yandex.ru>
2023-11-30 22:23:15 +0300

    Add role attribute to JWT token

--- backend/src/main/java/ru/itmo/backend/security/JwtTokenUtils.java
+++ backend/src/main/java/ru/itmo/backend/security/JwtTokenUtils.java
@@ -5,42 +5,62 @@ import java.util.Date;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
-//import java.security.KeyPair;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenUtils {
-	//private static final KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
 	private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 	
 	public String generateToken(Authentication authentication) {
+		Map<String, Object> claims = new HashMap<>();
+		List<String> rolesList = authentication.getAuthorities().stream()
+						.map(GrantedAuthority::getAuthority)
+						.toList();
+		claims.put("roles", rolesList);
 		String username = authentication.getName();
 		Date currentDate = new Date();
 		Date expireDate = new Date(currentDate.getTime() + SecurityLiterals.JWT_EXPIRATION);
-		
-		String token = Jwts.builder()
+
+        return Jwts.builder()
+				.setClaims(claims)
 				.setSubject(username)
 				.setIssuedAt( new Date())
 				.setExpiration(expireDate)
 				.signWith(key,SignatureAlgorithm.HS512)
 				.compact();
-		System.out.println("New token :");
-		System.out.println(token);
-		return token;
 	}
-	public String getUsernameFromJWT(String token){
-		Claims claims = Jwts.parserBuilder()
+	private Claims getAllClaimsFromToken(String token) {
+		return Jwts.parserBuilder()
 				.setSigningKey(key)
 				.build()
 				.parseClaimsJws(token)
 				.getBody();
-		return claims.getSubject();
 	}
+	public String getUsernameFromJWT(String token) {
+		return getAllClaimsFromToken(token).getSubject();
+	}
+	public List<String> getRolesFromJWT(String token) {
+		return getAllClaimsFromToken(token).get("roles", List.class);
+	}
+//	public String getUsernameFromJWT(String token){
+//		Claims claims = Jwts.parserBuilder()
+//				.setSigningKey(key)
+//				.build()
+//				.parseClaimsJws(token)
+//				.getBody();
+//		return claims.getSubject();
+//	}
 	
 	public boolean validateToken(String token) {
 		try {
